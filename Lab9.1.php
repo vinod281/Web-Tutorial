456a8ca4126f93f6a510b25e4313fb687f8d6573
vinod281 <57266038+vinod281@users.noreply.github.com>
2023-07-31 21:47:24 +0530

    Add files via upload

---
+++ Lab9.1.php
@@ -0,0 +1,21 @@
+
+<?php
+// Create variables x and y and assign values
+$x = 6;
+$y = 4;
+
+// Perform calculations
+$sum = $x + $y;
+$difference = $x - $y;
+$product = $x * $y;
+$division = $x / $y;
+
+// Display the results in the specified format
+echo "Sum: " . $sum . "<br>";
+echo "Difference: " . $difference . "<br>";
+echo "Product: " . $product . "<br>";
+echo "Division: " . $division . "<br>";
+?>
+
+
+
